d9d5602451f0cce75239a3313dca6cc178d3b449
sebastian.homsi <sebastian.homsi@EZE1-LLN-B02942>
2022-10-18 09:49:31 -0300

    Complete Setup Blazor Server App and Authentication

---
+++ BookStoreApp.Blazor.Server.UI/Providers/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,69 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace BookStoreApp.Blazor.Server.UI.Providers
+{
+    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorageService;
+        private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler;
+
+        public ApiAuthenticationStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+            jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var savedToken = await localStorageService.GetItemAsync<string>("accessToken");
+            var user = new ClaimsPrincipal(new ClaimsIdentity());
+
+            if(savedToken == null)
+            {
+                return new AuthenticationState(user);
+            }
+
+            var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+
+            if(tokenContent.ValidTo < DateTime.Now)
+            {
+                return new AuthenticationState(user);
+            }
+
+            var claims = await GetClaims();
+
+            user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+
+            return new AuthenticationState(user);
+        }
+
+        public async Task LoggedIn()
+        {
+            var claims = await GetClaims();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+            var authState = Task.FromResult(new AuthenticationState(user));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public async Task LoggedOut()
+        {
+            await localStorageService.RemoveItemAsync("accessToken");
+            var nobody = new ClaimsPrincipal(new ClaimsIdentity());
+            var authState = Task.FromResult(new AuthenticationState(nobody));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private async Task<List<Claim>> GetClaims()
+        {
+            var savedToken = await localStorageService.GetItemAsync<string>("accessToken");
+            var tokenContent = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+            var claims = tokenContent.Claims.ToList();
+            claims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+            return claims;
+        }
+    }
+}
---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.LocalStorage;
+using BookStoreApp.Blazor.Server.UI.Providers;
+using BookStoreApp.Blazor.Server.UI.Services.Base;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient httpClient;
+        private readonly ILocalStorageService localStorageService;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+
+        public AuthenticationService(IClient httpClient, ILocalStorageService localStorageService,
+            AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.httpClient = httpClient;
+            this.localStorageService = localStorageService;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public async Task<bool> AuthenticateAsync(LoginUserDto loginUserDto)
+        {
+            var response = await httpClient.LoginAsync(loginUserDto);
+
+            //Store token
+            await localStorageService.SetItemAsync("accessToken", response.Token);
+
+            //Change auth state of app
+            await ((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedIn();
+
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            await((ApiAuthenticationStateProvider)authenticationStateProvider).LoggedOut();
+
+        }
+    }
+}
